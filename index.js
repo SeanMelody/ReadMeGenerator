da30ddb55bbbf04e33de6c34007f3dd248ef2325
Sean Melody <seandmelody@gmail.com>
2020-11-23 19:47:20 -0800

    Added node modules and GitIgnore

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of this project?: "
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe this project?: "
+    },
+    {
+        type: "input",
+        name: "tableOfContents",
+        message: "Please enter the Table of Contents: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How would you install this project?: "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How would you use this project?: "
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "please pick a license: "
+    },
+    {
+        type: "input",
+        name: "contribut",
+        message: "What are the contribution guidelines?: "
+    },
+    {
+        type: "input",
+        name: "testInst",
+        message: "Any test instructions?: "
+    },
 
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,39 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  
+  ## Description:
+    ${description}
+  
+  ## Table of Contents:
+    <a href src= "#Description">Description</a>
+    <a href src= "#TableOfContents">Table of Contents</a>
+    <a href src= "#Installation">Installation Instructions</a>
+    <a href src= "#Usage">Usage</a>
+    <a href src= "#License">License</a>
+    <a href src= "#contribute">Contribution Guidelines</a>
+    <a href src= "#TestInstructions">Test Instructions</a>
+    <a href src= "#Questions">Questions</a>
+  
+  ## Installation Instructions:
+  ${installation}
+  
+  ## Usage:
+  ${usage}
+  
+  ## License: 
+  ${license}
+
+  ## Contribution Guidelines: 
+  ${contribute}
+
+  ## Test Instructions: 
+  ${testInst}
+  
+  ## Questions:
+  Check out my GitHub: https://github.com/${github}
+  Or if you have any questions email me: ${email}
+
 `;
 }
 
