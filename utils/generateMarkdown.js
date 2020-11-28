fd1d4e3d4429d53a80d91c674504453d4e1fbde0
Sean Melody <seandmelody@gmail.com>
2020-11-28 13:13:15 -0800

    Adjusted ReadMe Display

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -23,42 +23,60 @@ function generateMarkdown(data) {
   }
 
 
-  return `# ${data.title}
-
-  
-  ## Description:
-    ${data.description}
-  
-  ## Table of Contents:
-    <a href src= "#Description">Description</a>
-    <a href src= "#TableOfContents">Table of Contents</a>
-    <a href src= "#Installation">Installation Instructions</a>
-    <a href src= "#Usage">Usage</a>
-    <a href src= "#License">License</a>
-    <a href src= "#contribute">Contribution Guidelines</a>
-    <a href src= "#TestInstructions">Test Instructions</a>
-    <a href src= "#Questions">Questions</a>
-  
-  ## Installation Instructions:
-  ${data.installation}
-  
-  ## Usage:
-  ${data.usage}
-  
-  ## License: 
+  return `# ${data.title} 
   ${licenseShield}
-
-  ## Contribution Guidelines: 
-  ${data.contribute}
-
-  ## Test Instructions: 
-  ${data.testInst}
   
-  ## Questions:
-  Check out my GitHub: https://github.com/${data.github}
-  Or if you have any questions email me: ${data.email}
-
-`;
+  ## Description
+      
+      ${data.description}
+  
+  
+  ## Table of Contents
+  
+  [Description](##Description)
+  
+  [Installation](##Installation)
+  
+  [Usage](##Usage)
+  
+  [License](##License)
+  
+  [Contributing](##Contributing)
+  
+  [Tests](##Tests)
+  
+          
+  ## Installation
+  
+      ${data.installation}
+          
+  ## Usage
+  
+      ${data.usage}
+          
+  ## License
+      
+      This application is covered under the ${data.license} License
+  
+  ## Contributing
+  
+      ${data.contribute}
+  
+  ## Tests
+  
+      ${data.testInst}
+          
+  
+  Questions?
+  
+  Check out my GitHub:
+  
+  [github.com/${data.github}](https://github.com/${data.github})
+  
+  Or if you have any questions email me: 
+  
+  ${data.email}`
+    ;
 }
 
 module.exports = generateMarkdown;
