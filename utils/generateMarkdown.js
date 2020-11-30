f88960885cc907cd39a25f9cfa84f7148b194331
Sean Melody <seandmelody@gmail.com>
2020-11-29 21:17:43 -0800

    Synched formatting for Licence Shields

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,15 +5,15 @@ function generateMarkdown(data) {
 
   // If statement for if Apache License is picked
   if (data.license === "Apache") {
-    licenseShield = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    licenseShield = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
   }
   // If statement for if MIT License is picked
   if (data.license === "MIT") {
-    licenseShield = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    licenseShield = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
   }
   // If statement for if Mozilla License is picked
   if (data.license === "Mozilla") {
-    licenseShield = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0"
+    licenseShield = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
   }
   // If statement for if Unlicense License is picked
   if (data.license === "Unlicense") {
